e4aeed5cca2775956ee143f516d621d812441dbf
zekroTJA <riho@live.de>
2023-06-28 19:45:39 +0000

    add run script

---
+++ scripts/run.sh
@@ -0,0 +1,23 @@
+function run_day {
+    echo "--------- $1 ---------"
+    ./zig-out/bin/$1
+}
+
+zig build
+
+day=$1
+
+[ "$day" == "all" ] && {
+    days=$(ls -1 src | grep "day_")
+    for day in $days; do
+        run_day $day
+    done
+    
+    exit 0
+}
+
+[ -z "$DAY" ] && {
+    day=$(ls -r1 src | grep "day_" | head -1)
+}
+
+run_day $day
